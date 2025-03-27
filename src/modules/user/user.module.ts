151179f5898133619d7827f558fe63c4b5091de3
unknown <ddat888@gmail.com>
2025-03-27 13:39:25 +0700

    feat(mail): create mail sender module

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -4,6 +4,7 @@ import { UserController } from "./user.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "src/entities/user.entity";
 import { RedisModule } from "../cache/cache.module";
+import { MailModule } from "../mail/mail.module";
 
 @Module({
 	imports: [TypeOrmModule.forFeature([User]), RedisModule],
