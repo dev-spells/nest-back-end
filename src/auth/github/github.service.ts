80fb869ed082374c7c782d3b819bf5ac4f3d9af2
unknown <ddat888@gmail.com>
2025-04-01 21:46:24 +0700

    refactor(auth): refactor github login logic

--- src/auth/github/github.service.ts
+++ src/auth/github/github.service.ts
@@ -27,6 +27,8 @@ export class GithubService {
 			throw new ForbiddenException("User already exists with this email");
 		}
 		if (user && user.githubAccessToken) {
+			user.githubAccessToken = accessToken;
+			await this.userService.updateUser(user.id, user);
 			const { access_token, refresh_token } = this.createToken(user);
 			return res.redirect(
 				this.configService.get<string>("REDIRECT_URL") +
