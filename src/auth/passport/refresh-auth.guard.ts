2fa004666a75f2aae5b5c89faabfeb99dda66e39
unknown <ddat888@gmail.com>
2025-04-02 16:30:16 +0700

    refactor: change to constant error

--- src/auth/passport/refresh-auth.guard.ts
+++ src/auth/passport/refresh-auth.guard.ts
@@ -5,6 +5,8 @@ import {
 } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 
+import { TOKEN_ERRORS } from "src/constants/errors";
+
 @Injectable()
 export class RefreshAuthGuard extends AuthGuard("refresh-jwt") {
 	canActivate(context: ExecutionContext) {
@@ -16,7 +18,9 @@ export class RefreshAuthGuard extends AuthGuard("refresh-jwt") {
 	handleRequest(err, user, info) {
 		// You can throw an exception based on either "info" or "err" arguments
 		if (err || !user) {
-			throw err || new UnauthorizedException("Invalid refresh token");
+			throw (
+				err || new UnauthorizedException(TOKEN_ERRORS.INVALID_REFRESH_TOKEN)
+			);
 		}
 		return user;
 	}
