3bac6a674369f5ac37b3aa0e037579d168ffaba4
unknown <ddat888@gmail.com>
2025-04-13 10:39:14 +0700

    fix(course): delete exercise after delete course

--- src/modules/course/course.service.ts
+++ src/modules/course/course.service.ts
@@ -10,6 +10,10 @@ import { COURSE_ERRORS } from "src/constants/errors";
 import { Course } from "src/entities/course.entity";
 import { extractS3Key } from "src/utils/extract-s3-key.util";
 
+import { CodingExerciseService } from "../exercise/coding-exercise.service";
+import { MultipleChoiceExerciseService } from "../exercise/multiple-choice-exercise.service";
+import { QuizExerciseService } from "../exercise/quiz-exercise.service";
+
 import { S3Service } from "./../s3/s3.service";
 import { CreateCourseDto } from "./dto/create-course.dto";
 import { UpdateCourseDto } from "./dto/update-course.dto";
@@ -20,6 +24,9 @@ export class CourseService {
 		@InjectRepository(Course)
 		private courseRepository: Repository<Course>,
 		private s3Service: S3Service,
+		private codingExerciseService: CodingExerciseService,
+		private multipleChoiceExerciseService: MultipleChoiceExerciseService,
+		private quizExerciseService: QuizExerciseService,
 	) {}
 
 	async create(createCourseDto: CreateCourseDto) {
@@ -174,10 +181,33 @@ export class CourseService {
 	}
 
 	async remove(id: number) {
-		const course = await this.courseRepository.findOne({ where: { id } });
+		const course = await this.courseRepository.findOne({
+			where: { id },
+			relations: {
+				chapters: {
+					lessons: true,
+				},
+			},
+		});
 		if (!course) {
 			throw new NotFoundException(COURSE_ERRORS.NOT_FOUND);
 		}
+		for (const chapter of course.chapters) {
+			for (const lesson of chapter.lessons) {
+				if (lesson.codingExerciseId) {
+					this.codingExerciseService.deleteExercise(lesson.codingExerciseId);
+				}
+				if (lesson.multipleChoiceExerciseId) {
+					this.multipleChoiceExerciseService.delete(
+						lesson.multipleChoiceExerciseId,
+					);
+				}
+				if (lesson.quizExerciseId) {
+					this.quizExerciseService.delete(lesson.quizExerciseId);
+				}
+			}
+		}
+
 		const s3Key = extractS3Key(course.iconUrl);
 		if (s3Key) {
 			this.s3Service.deleteFile(s3Key);
