dafb79fa01dcae21806db392eb6c6acf917ff56f
unknown <ddat888@gmail.com>
2025-04-15 22:02:35 +0700

    feat(item-protect): initial to work with item protect

--- src/modules/item-protect-streak/item-protect-streak.controller.ts
+++ src/modules/item-protect-streak/item-protect-streak.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
-import { ItemProtectStreakService } from './item-protect-streak.service';
+import { Controller } from "@nestjs/common";
+import { Cron } from "@nestjs/schedule";
 
-@Controller('item-protect-streak')
+import { ApiTags } from "@nestjs/swagger";
+
+import { ItemProtectStreakService } from "./item-protect-streak.service";
+
+@ApiTags("item-protect-streak")
+@Controller("item-protect-streak")
 export class ItemProtectStreakController {
-  constructor(private readonly itemProtectStreakService: ItemProtectStreakService) {}
+	constructor(
+		private readonly itemProtectStreakService: ItemProtectStreakService,
+	) {}
+
+	@Cron("59 23 * * *")
+	async handleCron() {
+		console.log("Running cron job to protect streaks...");
+	}
 }
